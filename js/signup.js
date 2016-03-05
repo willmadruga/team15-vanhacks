f853d0377d3fd4d1ddc9550c9c570b7e07a13f14
Wong, Jesslly <jesslly.wong@netsuite.com>
2016-03-05 15:57:16 -0800

    add firebase can link, add signup html and js

--- js/signup.js
+++ js/signup.js
@@ -1,4 +1,4 @@
-angular.module('app.signUp', ['ngRoute'])
+angular.module('app.signUp', ['ngRoute', 'firebase'])
 
 .config(['$routeProvider', function($routeProvider) {
   
@@ -9,7 +9,49 @@ angular.module('app.signUp', ['ngRoute'])
   
 }])
 
-.controller('SignUpCtrl', function() {
-		console.log('sign up controller.');
-	}
-);
+.controller('SignUpCtrl', function($scope) {
+		console.log("signup ctrl");
+	
+var dbRef = new Firebase("https://knackio.firebaseio.com");
+
+      $scope.submitting = false;
+
+      $scope.createUserData = {};
+
+      $scope.alertMessage = '';
+
+      $scope.message = '';
+
+      $scope.createUser = function (createUserData) {
+        if (createUserData.confirmPassword == createUserData.password && !$scope.submitting) {
+
+          $scope.submitting = true;
+          console.log(" password is the same as password");
+          dbRef.createUser({
+            email: createUserData.email,
+            password: createUserData.password
+          }, function(error, userData) {
+            if (error) {
+              console.log("done loading - error");
+              $scope.alertMessage = "Error creating user";
+              $scope.message = "";
+             
+            } else {
+              
+              $scope.alertMessage = "";
+              $scope.message = "Successfully created user";
+              
+              console.log("done loading - success");
+            }
+
+            $scope.submitting = false;
+            $scope.$apply();
+          });
+
+        } else {
+          console.log("confirm password is not the same as password");
+          $scope.alertMessage = "Confirm password is not the same as password";
+          $scope.message = "";
+        }
+      }
+  });
