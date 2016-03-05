a5f42d49a8d76a9761973a30735ba026ad2118ee
William Madruga <wmadruga@monexa.com>
2016-03-05 12:14:53 -0800

    Initial angular templating setup - WIP

--- main.js
+++
@@ -1,9 +0,0 @@
-/* global angular */
-angular
-	.module('KnackApp', ["firebase"])
-  .controller('MainController', ['$scope', function ($scope) {
-  	 var dbRef = new Firebase("https://knackio.firebaseio.com");
-
-
-  }
-]);
