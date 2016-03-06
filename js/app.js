bfb1fc7dafa70fe7fd75607e7427264c28f12e69
Wong, Jesslly <jesslly.wong@netsuite.com>
2016-03-06 10:30:41 -0800

    style add tasks page

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ angular.module('app',
      'app.signIn',
      'app.signUp',
      'app.resetpwd',
+     'app.tasks',
      'app.profile'])
     .config(function ($routeProvider) {
         $routeProvider.
