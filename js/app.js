a42cce4db4eaee2ac0b23e7752bfa318da71f6dd
Anaid Gakhokidze <annygakhokidze@gmail.com>
2016-03-06 13:58:55 -0800

    stuff

--- js/app.js
+++ js/app.js
@@ -34,6 +34,19 @@ angular.module('app',
             $scope.$apply();
 
         }
+        $rootScope.getCurrentUser = function() {
+            var ref = new Firebase("https://knackio.firebaseio.com/");
+        
+            var authData = ref.getAuth();
+            if (authData == null) return;
+            if (authData.uid === "7ec090e1-a64f-438c-ac77-775b7a0240d0") {
+                  return "Agency";
+              } else if (authData.uid === "a90908a6-8638-4b63-a994-76c2f30e5384") {
+                  return "Earner";
+              } else if (authData.uid === "b77b8a3b-46fe-49de-bff0-122786a3bf09") {
+                  return "Employer";
+              }
+}
     })
     .controller('HomeCtrl', function () {
         console.log('home controller');
